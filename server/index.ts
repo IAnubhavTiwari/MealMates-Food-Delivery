d872ab46c4f4a3db6974ce6d3d3aed95e9511726
IAnubhavTiwari <anubhavtiwarionline@gmail.com>
2025-03-18 01:06:38 +0530

    added user controller and restaurant controller

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,32 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import connectDB from './db/connectDB';
+import userRoute from './routes/user.route';
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+
+// default middleware for any mern project
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+    origin: "https://localhost:5173",
+    credentials: true
+}
+app.use(cors(corsOptions))
+
+
+//api
+app.use("/api/v1/user", userRoute);
+
+
 
 app.listen(PORT, () => {
   connectDB();
