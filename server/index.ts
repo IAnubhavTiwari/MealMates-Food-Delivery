2d4af6841a602fcee9418cb6e07814f13afaf9be
IAnubhavTiwari <anubhavtiwarionline@gmail.com>
2025-03-18 11:31:19 +0530

    order controller and route

--- server/index.ts
+++ server/index.ts
@@ -7,6 +7,8 @@ import connectDB from './db/connectDB';
 import userRoute from './routes/user.route';
 import restaurantRoute from './routes/restaurant.route';
 import menuRoute from './routes/menu.route';
+import orderRoute from './routes/order.route';
+
 dotenv.config();
 const app = express();
 
@@ -29,6 +31,7 @@ app.use(cors(corsOptions))
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/restaurant", restaurantRoute);
 app.use("/api/v1/menu", menuRoute);
+app.use("/api/v1/order", orderRoute);
 
 
 
